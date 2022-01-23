a38dc69f0494fb76721c256bdea11a8f5f4d9476
Alex <younsuk.dory@gmail.com>
2022-01-22 19:39:47 -0500

    Added Prework files to repository

---
+++ Prework-intro-mobile/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Prework-intro-mobile
+//
+//  Created by Alex Kim on 1/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var NewText: UITextField!
+    @IBOutlet weak var BackgroundView: UIView!
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        TextLabel.textColor = UIColor.orange
+    }
+    @IBAction func BackgroundChange(_ sender: Any) {
+        BackgroundView.backgroundColor = UIColor.brown
+    }
+    
+    @IBAction func GoodBye(_ sender: Any) {
+        TextLabel.text = "GoodBye!"
+    }
+    @IBAction func Default(_ sender: Any) {
+        TextLabel.text = "Hello World!"
+        TextLabel.textColor = UIColor.black
+        BackgroundView.backgroundColor = UIColor.white
+    }
+    @IBAction func ChangeCustom(_ sender: Any) {
+        if let text = NewText.text, text.isEmpty {
+              TextLabel.text = "Hello World!"
+           } else {
+               TextLabel.text = NewText.text
+           }
+    }
+}
+
